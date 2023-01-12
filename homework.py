ca4657623531c8dd6d5bf37b2172bbf35c68bf6c
Evgenia Tarasova <etarasova@live.ru>
2023-01-12 00:28:20 -0800

    Tested

--- homework.py
+++ homework.py
@@ -13,10 +13,10 @@ class InfoMessage:
     calories: float
 
     message_info = ('Тип тренировки: {training_type}; '
-                        'Длительность: {duration:.3f} ч.; '
-                        'Дистанция: {distance:.3f} км; '
-                        'Ср. скорость: {speed:.3f} км/ч; '
-                        'Потрачено ккал: {calories:.3f}.')
+                    'Длительность: {duration:.3f} ч.; '
+                    'Дистанция: {distance:.3f} км; '
+                    'Ср. скорость: {speed:.3f} км/ч; '
+                    'Потрачено ккал: {calories:.3f}.')
 
     def get_message(self) -> str:
         return InfoMessage.message_info.format(**asdict(self))
